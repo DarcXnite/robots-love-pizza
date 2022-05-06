5cab3bab82004f2e5b171f4240de12618f2c2683
DarcXnite <anhri89@yahoo.com>
2022-05-06 16:04:10 -0500

    created canvas, created sprite class, created timmy, created drone, added gravity and velocity

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const gravity = 0.2;
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, 80, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const timmy = new Sprite({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+// timmy.draw();
+
+const drone = new Sprite({
+  position: { x: 400, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+// drone.draw();
+
+console.log(timmy);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  timmy.update();
+  drone.update();
+}
+
+animate();
