e66d3b03060e84453deca347890a4195203f2278
DarcXnite <anhri89@yahoo.com>
2022-05-12 12:56:44 -0500

    added killcount and idle and run sprites

--- js/classes.js
+++ js/classes.js
@@ -1,64 +1,143 @@
 class Sprite {
-  constructor({ position, imageSrc }) {
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
     this.position = position;
     this.width = 80;
     this.height = 150;
     this.image = new Image();
     this.image.src = imageSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+    this.offset = offset;
   }
 
   draw() {
-    ctx.drawImage(this.image, this.position.x, this.position.y);
+    ctx.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
   }
 
   update() {
     this.draw();
+    this.framesElapsed++;
+
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
+    }
   }
 }
 
-class Hero {
-  constructor({ position, velocity, color, offset }) {
-    this.position = position;
+class Hero extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+    sprites,
+    attackBox = { offset: {}, width: undefined, height: undefined },
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset,
+    });
+
     this.velocity = velocity;
-    this.width = 80;
-    this.height = 150;
+    this.width = 50;
+    this.height = 70;
     this.lastKey;
     this.attackBox = {
       position: { x: this.position.x, y: this.position.y },
-      offset,
-      width: 180,
-      height: 100,
+      offset: attackBox.offset,
+      width: attackBox.width,
+      height: attackBox.height,
     };
     this.color = color;
     this.isAttacking;
     this.isJumping;
     this.jumpCount = 0;
     this.heroHealth = 3;
-  }
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+    this.sprites = sprites;
 
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.position.x, this.position.y, 80, this.height);
-
-    // drawing the attack box
-
-    if (this.isAttacking) {
-      ctx.fillStyle = "pink";
-      ctx.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height
-      );
+    for (const sprite in this.sprites) {
+      sprites[sprite].image = new Image();
+      sprites[sprite].image.src = sprites[sprite].imageSrc;
     }
   }
 
+  // draw() {
+  //   ctx.fillStyle = this.color;
+  //   ctx.fillRect(this.position.x, this.position.y, 80, this.height);
+
+  //   // drawing the attack box
+
+  //   if (this.isAttacking) {
+  //     ctx.fillStyle = "pink";
+  //     ctx.fillRect(
+  //       this.attackBox.position.x,
+  //       this.attackBox.position.y,
+  //       this.attackBox.width,
+  //       this.attackBox.height
+  //     );
+  //   }
+  // }
+
   update() {
     this.draw();
+    this.framesElapsed++;
+
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
+    }
 
     // attack box to stay with character
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
     this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+
+    // draws out the square for the player
+    // ctx.fillStyle = "red";
+    // ctx.fillRect(this.position.x, this.position.y, 80, this.height);
+
+    // draws out the attack box
+    // ctx.fillStyle = "pink";
+    // ctx.fillRect(
+    //   this.attackBox.position.x,
+    //   this.attackBox.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // );
 
     // movement calculation
     this.position.x += this.velocity.x;
--- script.js
+++ script.js
@@ -26,6 +26,31 @@ const timmy = new Hero({
   velocity: { x: 0, y: 0 },
   color: "red",
   offset: { x: 50, y: 20 },
+  imageSrc: "./images/sprites/timmy-idle.png",
+  framesMax: 6,
+  scale: 3,
+  offset: {
+    x: 100,
+    y: 124,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./images/sprites/timmy-idle.png",
+      framesMax: 6,
+    },
+    run: {
+      imageSrc: "./images/sprites/timmy-run.png",
+      framesMax: 6,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    width: 100,
+    height: 50,
+  },
 });
 
 const drone = new Enemy({
@@ -75,74 +100,79 @@ const spawnDrone = () => {
 };
 
 let heroHealth = 3;
+let killCount = 0;
 
 // runs the refresh loop just like gameloop
 const animate = () => {
   window.requestAnimationFrame(animate);
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
-  if (heroHealth <= 0) {
+  if (heroHealth <= 0 || killCount === 50) {
     return;
   }
   timmy.update();
   // drone.update();
 
   // spawn drones
-  drones.forEach((drone, index) => {
-    drone.update();
+  // drones.forEach((drone, index) => {
+  //   drone.update();
 
-    // attack collision detection
-    if (
-      attackBoxCollision({ rect1: timmy, rect2: drone }) &&
-      timmy.isAttacking
-    ) {
-      // ensure that only 1 hit per attack press
-      timmy.isAttacking = false;
-      drone.isAlive = false;
-      if (!drone.isAlive) {
-        console.log("destroyed");
-        drones.splice(index, 1);
-      }
-      console.log("attacking");
-    }
+  //   // attack collision detection
+  //   if (
+  //     attackBoxCollision({ rect1: timmy, rect2: drone }) &&
+  //     timmy.isAttacking
+  //   ) {
+  //     // ensure that only 1 hit per attack press
+  //     timmy.isAttacking = false;
+  //     drone.isAlive = false;
+  //     if (!drone.isAlive) {
+  //       console.log("destroyed");
+  //       killCount++;
+  //       drones.splice(index, 1);
+  //     }
+  //     console.log("attacking");
+  //   }
 
-    // enemy attack collision detecton
-    if (
-      attackBoxCollision({ rect1: drone, rect2: timmy }) &&
-      drone.isAttacking
-    ) {
-      // ensure that only 1 hit per attack press
-      drone.isAttacking = false;
-      heroHealth--;
-      document.querySelector("#heroHealth").innerText = heroHealth;
-    }
+  //   // enemy attack collision detecton
+  //   if (
+  //     attackBoxCollision({ rect1: drone, rect2: timmy }) &&
+  //     drone.isAttacking
+  //   ) {
+  //     // ensure that only 1 hit per attack press
+  //     drone.isAttacking = false;
+  //     heroHealth--;
+  //     document.querySelector("#heroHealth").innerText = heroHealth;
+  //   }
 
-    if (drone.inRange) {
-      drone.velocity.x = 0;
-      return;
-    } else {
-      if (timmy.position.x <= drone.position.x) {
-        drone.position.x -= 2;
-      }
+  //   if (drone.inRange) {
+  //     drone.velocity.x = 0;
+  //     return;
+  //   } else {
+  //     if (timmy.position.x <= drone.position.x) {
+  //       drone.position.x -= 2;
+  //     }
 
-      if (timmy.position.x >= drone.position.x) {
-        drone.position.x += 2;
-      }
-    }
+  //     if (timmy.position.x >= drone.position.x) {
+  //       drone.position.x += 2;
+  //     }
+  //   }
 
-    if (timmy.velocity.y < 0) {
-      drone.velocity.y = -7;
-    }
-  });
+  //   if (timmy.velocity.y < 0) {
+  //     drone.velocity.y = -7;
+  //   }
+  // });
 
   // resets movement so sprite doesnt continuiously move
   timmy.velocity.x = 0;
 
   // movement checker, so the last key pressed will be the newest movement detected
+  timmy.image = timmy.sprites.idle.image;
   if (keys.ArrowLeft.pressed && lastKey === "ArrowLeft") {
     timmy.velocity.x = -5;
+    timmy.image = timmy.sprites.run.image;
   } else if (keys.ArrowRight.pressed && lastKey === "ArrowRight") {
     timmy.velocity.x = 5;
+    timmy.image = timmy.sprites.run.image;
   }
 
   // attack collision detection
